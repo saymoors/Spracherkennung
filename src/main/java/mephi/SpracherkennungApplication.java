8c26619a8cdf1ab9a9a4f1de88a7e3b246cbc5da
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-05-14 23:20:40 +0300

    Инициализация Spring Boot приложения
      Добавлен главный класс SpracherkennungApplication
      Включена @Async для фоновой обработки транскрипций
      Включена @Scheduled для периодического опроса статуса задач в SaluteSpeech
      Изменение pom.xml файла с добавлением зависимости для корректного запуска

---
+++ src/main/java/mephi/SpracherkennungApplication.java
@@ -0,0 +1,16 @@
+package mephi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+public class SpracherkennungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpracherkennungApplication.class, args);
+    }
+}
