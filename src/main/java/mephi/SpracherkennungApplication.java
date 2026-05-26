b09b9f7da6c7223d20fcb705bdd69e8f285099dc
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-05-26 19:30:37 +0300

    Корректировка SpracherkennungApplication.java
    Добавление возможности читать .env файл с секретами вместе с запуском программы. Необходимо запускать jar файл в одной папке с env. файлом для возможности его чтения

--- src/main/java/mephi/SpracherkennungApplication.java
+++ src/main/java/mephi/SpracherkennungApplication.java
@@ -2,15 +2,32 @@ package mephi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
-@EnableAsync
 @EnableScheduling
 public class SpracherkennungApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv
+                .configure()
+                .directory(".")
+                .ignoreIfMissing()
+                .load();
+
+        setProperty(dotenv, "JWT_SECRET");
+        setProperty(dotenv, "JWT_EXPIRATION_MS");
+        setProperty(dotenv, "SALUTESPEECH_CLIENT_ID");
+        setProperty(dotenv, "SALUTESPEECH_CLIENT_SECRET");
+
         SpringApplication.run(SpracherkennungApplication.class, args);
+    }
+
+    private static void setProperty(Dotenv dotenv, String key) {
+        String value = dotenv.get(key);
+        if (value != null) {
+            System.setProperty(key, value);
+        }
     }
 }
