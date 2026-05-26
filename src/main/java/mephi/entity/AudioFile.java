be49af8f85da8a132557fcee7dfccda6e31752f9
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-05-26 20:35:54 +0300

    Корректировка AudioFile.java
    Добавление полей сущности AudioFile, геттеров/сеттеров, а также связей с базой данных таких, как @Entity @Table @Id @Column

--- src/main/java/mephi/entity/AudioFile.java
+++ src/main/java/mephi/entity/AudioFile.java
@@ -1,5 +1,98 @@
 package mephi.entity;
 
-public class AudioFile {
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+@Entity
+@Table(name = "audio_files")
+public class AudioFile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_id", nullable = false)
+    private Integer userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String format;
+
+    @Column(name = "system_path", nullable = false)
+    private String systemPath;
+
+    @Column(name = "size_bytes", nullable = false)
+    private Long sizeBytes;
+
+    @Column(name = "sber_request_file_id")
+    private UUID sberRequestFileId;
+
+    @Column(name = "upload_at", nullable = false, updatable = false)
+    private LocalDateTime uploadAt = LocalDateTime.now();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getSystemPath() {
+        return systemPath;
+    }
+
+    public void setSystemPath(String systemPath) {
+        this.systemPath = systemPath;
+    }
+
+    public Long getSizeBytes() {
+        return sizeBytes;
+    }
+
+    public void setSizeBytes(Long sizeBytes) {
+        this.sizeBytes = sizeBytes;
+    }
+
+    public UUID getSberRequestFileId() {
+        return sberRequestFileId;
+    }
+
+    public void setSberRequestFileId(UUID sberRequestFileId) {
+        this.sberRequestFileId = sberRequestFileId;
+    }
+
+    public LocalDateTime getUploadAt() {
+        return uploadAt;
+    }
+
+    public void setUploadAt(LocalDateTime uploadAt) {
+        this.uploadAt = uploadAt;
+    }
 }
