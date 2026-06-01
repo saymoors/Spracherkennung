90144803fd220125c2321668806e63563521d6e7
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-06-01 12:54:45 +0300

    Заполнение JwtService.java
    Добавлены поля SecretKey key и Integer expirationMs, хранящие ключ и время действия токена соответственно. Также добавлен конструктор с этими полями. Созданы два метода: generateToken (для генерации токена из login, userId, текущей даты, времени действия токена, секретного ключа) и extractUserId (для извлечения userId из токена)

--- src/main/java/mephi/service/JwtService.java
+++ src/main/java/mephi/service/JwtService.java
@@ -1,5 +1,40 @@
 package mephi.service;
 
-public class JwtService {
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private final SecretKey key;
+    private final Integer expirationMs;
+
+    public JwtService(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration-ms}") Integer expirationMs) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.expirationMs = expirationMs;
+    }
+
+    public String generateToken(Integer userId, String login) {
+        return Jwts.builder()
+                .subject(login)
+                .claim("userId", userId)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(key)
+                .compact();
+    }
+
+    public Integer extractUserId(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("userId", Integer.class);
+    }
 }
