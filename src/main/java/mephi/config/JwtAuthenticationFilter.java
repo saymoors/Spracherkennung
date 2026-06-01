16a6d07950d9778da3264bfdfe35bfe618abe8ff
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-06-01 13:48:56 +0300

    Заполнение JwtAuthenticationFilter.java
    Переопределение метода doFilterInternal (создание фильтра для определения верного токена

--- src/main/java/mephi/config/JwtAuthenticationFilter.java
+++ src/main/java/mephi/config/JwtAuthenticationFilter.java
@@ -1,5 +1,37 @@
 package mephi.config;
 
-public class JwtAuthenticationFilter {
-    
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mephi.service.JwtService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            Integer userId = jwtService.extractUserId(token);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                    userId, null, Collections.emptyList()
+            );
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
