11d829d916ac15515d22770256b4e19034a0050e
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-06-01 09:40:36 +0300

    Заполнение ExternalCallLogRepository.java
    findTopByTranscriptionIdOrderByCreatedAtDesc(transcriptionId)
    Берет последний лог внешнего вызова для транскрипции. Нужен, чтобы при статусе ERROR показать пользователю последнюю понятную причину ошибки.

--- src/main/java/mephi/repository/ExternalCallLogRepository.java
+++ src/main/java/mephi/repository/ExternalCallLogRepository.java
@@ -1,5 +1,12 @@
 package mephi.repository;
 
-public interface ExternalCallLogRepository {
+import mephi.entity.ExternalCallLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
+public interface ExternalCallLogRepository extends JpaRepository<ExternalCallLog, Integer> {
+    Optional<ExternalCallLog> findTopByTranscriptionIdOrderByCreatedAtDesc(Integer transcriptionId);
 }
