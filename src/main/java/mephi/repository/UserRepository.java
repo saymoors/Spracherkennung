5d966ec5f94db52e1bc3d0a041351e164e26abc0
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-06-01 13:46:04 +0300

    Обновление UserRepository.java
    Замена Optional<User> на User. Простота логики работы в AuthService с User и null.

--- src/main/java/mephi/repository/UserRepository.java
+++ src/main/java/mephi/repository/UserRepository.java
@@ -4,11 +4,9 @@ import mephi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByLogin(String login);
+    User findByLogin(String login);
 
     boolean existsByLogin(String login);
 
