7017d0537585db2bc8449a492b4ae91e6f6883c1
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-06-01 09:41:33 +0300

    Заполнение SemanticBlockRepository.java
    findByTranscriptionIdOrderByOrderIndexAsc(transcriptionId)
    Ищет текстовые блоки конкретной транскрипции в правильном порядке. Используется для просмотра результата, экспорта в PDF, проверки что у DONE-транскрипции реально есть текст, и копирования готового результата в новую транскрипцию.

--- src/main/java/mephi/repository/SemanticBlockRepository.java
+++ src/main/java/mephi/repository/SemanticBlockRepository.java
@@ -1,5 +1,12 @@
 package mephi.repository;
 
-public interface SemanticBlockRepository {
+import mephi.entity.SemanticBlock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface SemanticBlockRepository extends JpaRepository<SemanticBlock, Integer> {
+    List<SemanticBlock> findByTranscriptionIdOrderByOrderIndexAsc(Integer transcriptionId);
 }
