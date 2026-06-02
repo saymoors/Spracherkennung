25f3981895b46beb41556fb751fe925c8d661f37
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-06-02 12:57:18 +0300

    Обновление AudioFileRepository.java
    Замена Optional<AudioFile> на AudioFile. Достигнута простота работы по моему личному мнению.

--- src/main/java/mephi/repository/AudioFileRepository.java
+++ src/main/java/mephi/repository/AudioFileRepository.java
@@ -5,11 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface AudioFileRepository extends JpaRepository<AudioFile, Integer> {
     List<AudioFile> findByUserIdAndFileHashOrderByUploadAtDesc(Integer userId, String fileHash);
 
-    Optional<AudioFile> findFirstByUserIdAndFileHashAndNameOrderByUploadAtDesc(Integer userId, String fileHash, String name);
+    AudioFile findFirstByUserIdAndFileHashAndNameOrderByUploadAtDesc(Integer userId, String fileHash, String name);
 }
