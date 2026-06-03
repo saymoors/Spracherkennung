556d17f18a4578f8618f81e22df27a8df8d71c06
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-06-03 20:27:33 +0300

    Обновление TranscriptionService.java
    Добавлена переменная RECOGNITION_LANGUAGE, означающая язык распознавания, все необходимые репозитории и сервисы: AudioFileService, AudioFileRepository, TranscriptionRepository, SemanticBlockRepository, ExternalCallLogRepository, ExportService, SaluteSpeechService. Также добавлен конструктор со всеми этими объектами.

--- src/main/java/mephi/service/TranscriptionService.java
+++ src/main/java/mephi/service/TranscriptionService.java
@@ -1,5 +1,33 @@
 package mephi.service;
 
-public class TranscriptionService {
+import mephi.repository.*;
+import org.springframework.stereotype.Service;
 
+@Service
+public class TranscriptionService {
+    private static final String RECOGNITION_LANGUAGE = "ru-RU";
+
+    private final AudioFileService audioFileService;
+    private final AudioFileRepository audioFileRepository;
+    private final TranscriptionRepository transcriptionRepository;
+    private final SemanticBlockRepository semanticBlockRepository;
+    private final ExternalCallLogRepository externalCallLogRepository;
+    private final ExportService exportService;
+    private final SaluteSpeechService saluteSpeechService;
+
+    public TranscriptionService(AudioFileService audioFileService,
+                                AudioFileRepository audioFileRepository,
+                                TranscriptionRepository transcriptionRepository,
+                                SemanticBlockRepository semanticBlockRepository,
+                                ExternalCallLogRepository externalCallLogRepository,
+                                ExportService exportService,
+                                SaluteSpeechService saluteSpeechService) {
+        this.audioFileService = audioFileService;
+        this.audioFileRepository = audioFileRepository;
+        this.transcriptionRepository = transcriptionRepository;
+        this.semanticBlockRepository = semanticBlockRepository;
+        this.externalCallLogRepository = externalCallLogRepository;
+        this.exportService = exportService;
+        this.saluteSpeechService = saluteSpeechService;
+    }
 }
